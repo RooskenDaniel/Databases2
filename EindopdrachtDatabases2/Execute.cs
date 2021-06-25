3f8fa9cc483fa70abe1e9487eeb4e02cc2bfadcd
Kariemdv <56026990+Kariemdv@users.noreply.github.com>
2021-06-25 13:45:19 +0200

    Start making execution code

---
+++ EindopdrachtDatabases2/Execute.cs
@@ -0,0 +1,28 @@
+﻿using EindopdrachtDatabases2.DatabaseConnections;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EindopdrachtDatabases2
+{
+    class Execute
+    {
+        private Stopwatch stopwatch;
+        private List<IDBConn> databases;
+        public Execute()
+        {
+
+        }
+
+        public void AddDatabaseToList(IDBConn db)
+        {
+            if (!databases.Contains(db) && db.IsConnected())
+                databases.Add(db);
+            else
+                throw new Exception("Database has not been added!");
+        }
+    }
+}
---
+++ EindopdrachtDatabases2/Profiler.cs
@@ -0,0 +1,34 @@
+﻿using EindopdrachtDatabases2.DatabaseConnections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EindopdrachtDatabases2
+{
+    class Profiler
+    {
+        private IDBConn adoConn;
+        private IDBConn mongoConn;
+        private IDBConn efConn;
+
+        public Profiler()
+        {
+            adoConn = new AdoNet();
+            //mongoConn = new Mongo();
+            //efConn = new EntityFramework();
+        }
+
+        public void StartProfiling()
+        {
+            Console.WriteLine("Let's start profiling some databases on their speed with CRUD-operations!\nAttempting to connect to all databases");
+
+            adoConn.Connect();
+            //mongoConn.Connect();
+            //efConn.Connect();
+
+            Console.WriteLine("Connected to all databases!");
+        }
+    }
+}
